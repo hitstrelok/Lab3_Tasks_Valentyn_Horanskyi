16cb2ae61229caf7ae60caaaf6c37677a61eb2a3
hitstrelok <hitstrelok@gmail.com>
2023-10-21 13:28:19 +0100

    Valentyn Horanskyi (ID:63500, group 1)

---
+++ Task_1_Input/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Valentyn Horanskyi (ID:63500, group 1)");
+        System.out.println("Please enter first number: ");
+        float firstNumber = scanner.nextInt();
+        System.out.println("Please enter second number: ");
+        float secondNumber = scanner.nextInt();
+
+
+        System.out.println("Sum(+): " + (firstNumber + secondNumber));
+
+        System.out.println("Difference(-): " + (firstNumber - secondNumber));
+
+        System.out.println("Product(*): " + (firstNumber * secondNumber));
+
+        System.out.println("Quotient(/): " + (firstNumber / secondNumber));
+
+
+        scanner.close();
+    }
+}
+
